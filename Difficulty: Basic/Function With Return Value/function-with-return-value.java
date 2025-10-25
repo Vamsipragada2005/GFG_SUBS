1ab0133f90aa946d9e6ac58c6bace4fd8f163c90
PRAGADA VAMSI <154915845+Vamsipragada2005@users.noreply.github.com>
2025-10-25 11:15:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Return Value/function-with-return-value.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int returnValueFunction(int n) {
+        return 2 * n; 
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(returnValueFunction(n)); 
+    }
+}
