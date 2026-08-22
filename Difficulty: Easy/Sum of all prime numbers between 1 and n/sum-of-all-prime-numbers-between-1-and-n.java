10bb1bd6df31d0e83b2e347da0422dbacb1e2e8d
PRAGADA VAMSI <154915845+Vamsipragada2005@users.noreply.github.com>
2026-08-22 17:02:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of all prime numbers between 1 and n/sum-of-all-prime-numbers-between-1-and-n.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int prime_Sum(int n) {
+        // code here
+        if(n<2)  return 0;
+      boolean [] isPrime = new boolean[n+1];
+      for(int i=2;i<=n;i++){
+          isPrime[i]=true;
+      }
+      for(int i=2;i*i<=n;i++){
+          if(isPrime[i]){
+              for(int j=i*i;j<=n;j+=i){
+                  isPrime[j]= false;
+              }
+              
+          }
+      }
+      int sum=0;
+      for(int i=2;i<=n;i++){
+          if(isPrime[i]){
+              sum+=i;
+          }
+      }
+      return sum;
+        
+        
+    }
+}
